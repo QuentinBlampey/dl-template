c845faa93842143707815f82516f854730470d1b
QuentinBlampey <quentin.blampey@gmail.com>
2021-08-23 10:57:58 +0200

    using wandb

--- src/train.py
+++ src/train.py
@@ -9,7 +9,7 @@ from pytorch_lightning import (
     Trainer,
     seed_everything,
 )
-from pytorch_lightning.loggers import LightningLoggerBase
+from pytorch_lightning.loggers import WandbLogger
 
 from src.utils import utils
 
@@ -48,12 +48,9 @@ def train(config: DictConfig) -> Optional[float]:
                 callbacks.append(hydra.utils.instantiate(cb_conf))
 
     # Init lightning loggers
-    logger: List[LightningLoggerBase] = []
-    if "logger" in config:
-        for _, lg_conf in config.logger.items():
-            if "_target_" in lg_conf:
-                log.info(f"Instantiating logger <{lg_conf._target_}>")
-                logger.append(hydra.utils.instantiate(lg_conf))
+    log.info(f"Instantiating logger <{config.logger._target_}>")
+    logger: WandbLogger = hydra.utils.instantiate(config.logger)
+
 
     # Init lightning trainer
     log.info(f"Instantiating trainer <{config.trainer._target_}>")
